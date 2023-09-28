b54a6ad8c8b90363862e13b59b29dfd657344144
nguyendangkhanhang <142975786+nguyendangkhanhang@users.noreply.github.com>
2023-09-28 16:28:15 +0700

    lab2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
   <head>
-    <title> PHP Test </title>title>
+    <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '>p>Hello Cloud Computing class!</p>; ?>
+    <?php echo '<p>Hello Cloud Computing class!</p>'; ?>
   </body>
 </html>
